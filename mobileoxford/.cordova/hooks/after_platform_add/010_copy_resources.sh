354b14b5c23315d336394bfca86d79200e850170
David King <dave@davbo.org>
2014-02-14 15:14:55 +0000

    Removed cordova hook for ios
    Handling this in Jenkins for now

--- mobileoxford/.cordova/hooks/after_platform_add/010_copy_resources.sh
+++ mobileoxford/.cordova/hooks/after_platform_add/010_copy_resources.sh
@@ -7,11 +7,3 @@ if [[ $CORDOVA_PLATFORMS == *android* ]]
 then
     cp -r $RESOURCES/android/drawable-* platforms/android/res
 fi
-if [[ $CORDOVA_PLATFORMS == *ios* ]]
-then
-    rm platforms/ios/Mobile\ Oxford/Resources/icons/*
-    rm platforms/ios/Mobile\ Oxford/Resources/splash/*
-
-    cp $RESOURCES/ios/splash/* platforms/ios/Mobile\ Oxford/Resources/splash/
-    cp $RESOURCES/ios/icons/* platforms/ios/Mobile\ Oxford/Resources/icons/
-fi
