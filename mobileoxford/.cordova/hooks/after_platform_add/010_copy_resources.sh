4f77d77a2cce50d6861be63fcf5e14a80999079e
David King <dave@davbo.org>
2014-02-18 12:10:33 +0000

    Copy iOS resources

--- mobileoxford/.cordova/hooks/after_platform_add/010_copy_resources.sh
+++ mobileoxford/.cordova/hooks/after_platform_add/010_copy_resources.sh
@@ -7,3 +7,11 @@ if [[ $CORDOVA_PLATFORMS == *android* ]]
 then
     cp -r $RESOURCES/android/drawable-* platforms/android/res
 fi
+
+if [[ $CORDOVA_PLATFORMS == *ios* ]]
+then
+    rm platforms/ios/Mobile\ Oxford/Resources/splash/*
+    rm platforms/ios/Mobile\ Oxford/Resources/icons/*
+    cp $RESOURCES/ios/icons/* platforms/ios/Mobile\ Oxford/Resources/icons/
+    cp $RESOURCES/ios/splash/* platforms/ios/Mobile\ Oxford/Resources/splash/
+fi
